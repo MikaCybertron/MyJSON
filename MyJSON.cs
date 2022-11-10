10234abccc22ffe14c4c7e34b39d1f0bd0201c2f
Mika Cybertron <45533977+MikaCybertron@users.noreply.github.com>
2022-11-10 15:14:16 +0700

    Add MyJSON.cs

---
+++ MyJSON.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+public class MyJSON
+{
+	public static void Create(object objectValue, string PathFile)
+	{
+		string jsonString = JsonConvert.SerializeObject(objectValue);
+
+		JObject jObject = JsonConvert.DeserializeObject(jsonString) as JObject;
+		
+		File.WriteAllText(PathFile, jObject.ToString());
+	}
+	
+	public static void Update(string KeyToken, JToken ReplaceValue, string PathFile)
+	{
+		if (File.Exists(PathFile))
+		{
+			string jsonString = File.ReadAllText(PathFile);
+			
+			JObject jObject = JsonConvert.DeserializeObject(jsonString) as JObject;
+			
+			JToken token = jObject.SelectToken(KeyToken);
+			
+			token.Replace(ReplaceValue);
+			
+			string updatedJsonString = jObject.ToString();
+			
+			File.WriteAllText(PathFile, updatedJsonString);
+		}
+	}
+}
