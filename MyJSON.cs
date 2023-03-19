2b885f5a3d685a4033c049bf8e0de9542ec4f322
Mika Cybertron <45533977+MikaCybertron@users.noreply.github.com>
2023-03-20 02:23:54 +0800

    Update MyJSON.cs

--- MyJSON.cs
+++ MyJSON.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -9,30 +9,44 @@ using Newtonsoft.Json.Linq;
 
 public class MyJSON
 {
-	public static void Create(object objectValue, string PathFile)
-	{
-		string jsonString = JsonConvert.SerializeObject(objectValue);
+    public static void CreateWithoutIndented(object objectValue, string PathFile)
+    {
+        string jsonString = JsonConvert.SerializeObject(objectValue);
 
-		JObject jObject = JsonConvert.DeserializeObject(jsonString) as JObject;
-		
-		File.WriteAllText(PathFile, jObject.ToString());
-	}
-	
-	public static void Update(string KeyToken, JToken ReplaceValue, string PathFile)
-	{
-		if (File.Exists(PathFile))
-		{
-			string jsonString = File.ReadAllText(PathFile);
-			
-			JObject jObject = JsonConvert.DeserializeObject(jsonString) as JObject;
-			
-			JToken token = jObject.SelectToken(KeyToken);
-			
-			token.Replace(ReplaceValue);
-			
-			string updatedJsonString = jObject.ToString();
-			
-			File.WriteAllText(PathFile, updatedJsonString);
-		}
-	}
+        File.WriteAllText(PathFile, jsonString);
+    }
+
+    public static void CreateWithIndented(object objectValue, string PathFile)
+    {
+        string jsonString = JsonConvert.SerializeObject(objectValue);
+
+        JObject jObject = JsonConvert.DeserializeObject(jsonString) as JObject;
+
+        File.WriteAllText(PathFile, jObject.ToString());
+    }
+
+    public static void CreateWithIndented(string jsonString, string PathFile)
+    {
+        JObject jObject = JsonConvert.DeserializeObject(jsonString) as JObject;
+
+        File.WriteAllText(PathFile, jObject.ToString());
+    }
+
+    public static void Update(string KeyToken, JToken ReplaceValue, string PathFile)
+    {
+        if (File.Exists(PathFile))
+        {
+            string jsonString = File.ReadAllText(PathFile);
+
+            JObject jObject = JsonConvert.DeserializeObject(jsonString) as JObject;
+
+            JToken token = jObject.SelectToken(KeyToken);
+
+            token.Replace(ReplaceValue);
+
+            string updatedJsonString = jObject.ToString();
+
+            File.WriteAllText(PathFile, updatedJsonString);
+        }
+    }
 }
